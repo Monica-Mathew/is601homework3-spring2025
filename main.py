4ea4e077d19b0e15de3f9fd64e73c1e9e82ccde0
Monica-Mathew <63261145+Monica-Mathew@users.noreply.github.com>
2025-02-18 19:39:35 -0500

    updated main file

--- main.py
+++ main.py
@@ -8,12 +8,24 @@ def calculate_and_print(a,b, operation_name):
         'subtract': Calculator.subtract,
         'multiply': Calculator.multiply,
         'divide': Calculator.divide
-    
     }
+    try:
+        a_decimal, b_decimal = map(Decimal, [a,b])
+        result = operation_mappings.get(operation_name)
+        if result:
+            print(f"The result of {a} {operation_name} {b} is equal to {result(a_decimal,b_decimal)}")
+        else:
+            print(f"Unknown operation : {operation_name}")
+    except InvalidOperation:
+         print(f"Invalid number input: {a} or {b} is not a valid number.")
+    except ZeroDivisionError:
+        print("Error :Division by zero")
+    except Exception as e:
+        print(f"An error occured : {e}")
 
 def main():
     if(len(sys.argv)) != 4:
-        print("Usage :python calculator _main.py <number1> <number2> <operation")
+        print("Usage :python calculator _main.py <number1> <number2> <operation>")
         sys.exit(1)
 
     _,a,b,operation = sys.argv
